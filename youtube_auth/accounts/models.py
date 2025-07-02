d6505da38d5920c0c76dd0d2e20f7f86ec99abb8
Ark123-git <ark62744@gmail.com>
2025-07-02 19:15:32 +0530

    Done

---
+++ youtube_auth/accounts/models.py
@@ -0,0 +1,14 @@
+# from django.db import models
+
+# # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+import uuid
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
---
+++ youtube_auth/accounts/views.py
@@ -0,0 +1,64 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RegisterForm
+from .models import UserProfile
+from django.contrib.auth.models import User
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            profile = UserProfile.objects.create(user=user)
+            return redirect(f'/verify/{profile.token}/')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def verify_view(request, token):
+    try:
+        profile = UserProfile.objects.get(token=token)
+        profile.is_verified = True
+        profile.user.is_active = True
+        profile.user.save()
+        profile.save()
+        # messages.success(request, 'Account Verified Successfully')
+        return render(request,'verify.html')
+    except:
+        return render(request, 'verify.html', {'error': 'Invalid token'})
+
+
+  
+   
+  
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            if hasattr(user, 'userprofile') and user.userprofile.is_verified:
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Account not verified.')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
